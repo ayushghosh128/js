032996f90d5b4a9c933f7057524341aa559e096e
ayushghosh128 <118094811+ayushghosh128@users.noreply.github.com>
2024-02-14 15:46:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//treats all js code as newer version
+
+//alert(3+3)using node js not browser
+
+//DATA TYPES
+//number
+//bigint
+//boolean
+//null=>standalone value
+//undefined=> value not assigned
+//symbol=>unique
+
+//object 
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
+//typeof keyword is used to get the type of value
