2126956545888531677cc527ee0f9c0806d0bc3d
ayushghosh128 <118094811+ayushghosh128@users.noreply.github.com>
2024-02-14 16:23:43 +0000

    summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,23 @@
+/*
+PRIMITIVE(7 types) passes by value not reference
+    String 
+    Number 
+    Boolean 
+    null 
+    undefined 
+    Symbol 
+    BigInt
+NON-PRIMITIVE(Reference)
+    Arrays
+    const names=["ayush","ashish","sahil"]
+
+    Objects
+    {
+        name:"ayush"
+        age:22
+    }
+    Functions
+    const myFunc=function(){
+        console.log("hello world")
+    }
+*/
